3ba99f469fd13e27ac8b7b9b3a4bb56cde4ca1e5
Mahadi Hasan Tanmoy <53210886+mhtanmoy@users.noreply.github.com>
2020-07-10 20:25:00 +0600

    Update auto comment.py

--- auto comment.py
+++ auto comment.py
@@ -6,6 +6,6 @@ comment = ["wow","cool capture","Rating 10/10","Winner here","Nice picture","Bes
 time.sleep(5)
 
 for i in range(10): #change range how much comment do you need.
-    pyautogui.typewrite(comment[1%7])
+    pyautogui.typewrite(comment[i%7])
     pyautogui.typewrite("\n")
     time.sleep(6)
